359250c643c994a48e676e5845ba7c0e81c9ee44
Rodney Wells <rodneywells01@gmail.com>
2024-12-08 01:57:50 -0600

    Day 6 part one

--- 2024/Day6/main.py
+++ 2024/Day6/main.py
@@ -1,1 +1,123 @@
-Hello, world!
+f = open("input.txt")
+data = list()
+for line in f:
+    data.append(line.rstrip())
+
+def load_map():
+    f = open("input.txt")
+    map = list()
+    for line in f:
+        map.append(list(line.rstrip()))
+    return map
+
+class GuardSimulation():
+    def __init__(self):
+        self.map = load_map()
+        self.velocity = {
+            "<": {
+                "x": -1,
+                "y": 0
+            },
+            ">": {
+                "x": 1,
+                "y": 0
+            },
+            "^": {
+                "x": 0,
+                "y": -1
+            },
+            "v": {
+                "x": 0,
+                "y": 1
+            }
+        }
+        self.locations_visited = 0
+
+        # Find the guard
+        guard_symbols = set(["<", ">", "^", "v"])
+        found = False
+        for row_idx in range(len(self.map)):
+            for col_idx in range(len(self.map[row_idx])):
+                if self.map[row_idx][col_idx] in guard_symbols:
+                    self.guard_row_idx = row_idx
+                    self.guard_col_idx = col_idx
+                    self.guard = self.map[row_idx][col_idx]
+                    found = True
+                    break
+
+            if found:
+                break
+
+        if not found:
+            raise Exception("Guard was not present in input!")
+
+
+    def is_done(self):
+        # Sim is done when guard is at edge and leaving.
+        return self.guard_row_idx == 0 and self.guard == "^" or \
+            self.guard_row_idx == len(self.map)-1 and self.guard == "v" or \
+            self.guard_col_idx == 0 and self.guard == "<" or \
+            self.guard_row_idx == len(self.map[self.guard_row_idx])-1 and self.guard == ">"
+
+
+    def rotate_guard(self):
+        # Guard always rotates right
+        next_position = {
+            "<": "^",
+            "^": ">",
+            ">": "v",
+            "v": "<"
+        }
+
+        self.guard = next_position[self.guard]
+
+    def display_map(self):
+        print()
+        print("==========")
+        for row in self.map:
+            print(row)
+        print("==========")
+        print()
+
+    def move_guard(self):
+        next_row_idx = self.guard_row_idx + self.velocity[self.guard]["y"]
+        next_col_idx = self.guard_col_idx + self.velocity[self.guard]["x"]
+        next_position = self.map[next_row_idx][next_col_idx]
+        if next_position == "#":
+            # Obstacle hit.
+            self.rotate_guard()
+        else:
+            # Mark current position as visited
+            self.map[self.guard_row_idx][self.guard_col_idx] = "X"
+
+            # Move forward
+            self.map[next_row_idx][next_col_idx] = self.guard
+            self.guard_row_idx = next_row_idx
+            self.guard_col_idx = next_col_idx
+
+    def calculate_unique_locations_visited(self):
+        total = 1 # Guard counts as a location visited
+        for row in self.map:
+            for col in row:
+                if col == "X":
+                    total += 1
+        return total
+
+
+    def run_simulation(self):
+        steps = 0
+        while not self.is_done():
+            self.move_guard()
+            steps += 1
+            print(steps)
+
+        self.display_map()
+        print(steps)
+        print(f"Visited {self.calculate_unique_locations_visited()} locations")
+
+        print("Done!")
+
+
+
+sim = GuardSimulation()
+sim.run_simulation()
